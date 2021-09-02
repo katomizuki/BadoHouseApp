81f0b0b4c51c372ce4aac7ab10bbd0d9fa359040
Katopan <76249494+katomizuki@users.noreply.github.com>
2021-09-02 09:29:09 +0900

    Initial Commit

---
+++ BadHouseApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BadHouseApp
+//
+//  Created by ミズキ on 2021/09/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
