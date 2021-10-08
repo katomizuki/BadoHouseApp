b1023160560928346785001a02384e492411c776
Katopan <76249494+katomizuki@users.noreply.github.com>
2021-10-08 11:09:07 +0900

    refectoring

--- BadHouseApp/Resource/AppDelegate.swift
+++ BadHouseApp/Resource/AppDelegate.swift
@@ -7,7 +7,7 @@ import FBSDKCoreKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
@@ -17,36 +17,36 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         setupNotifications(on: application)
         UIApplication.shared.applicationIconBadgeNumber = 0
         
-//        do {
-//            try Auth.auth().signOut()
-//        } catch {
-//
-//        }
+        //        do {
+        //            try Auth.auth().signOut()
+        //        } catch {
+        //
+        //        }
         
         return true
     }
- 
+    
     func application(_ application: UIApplication, handleOpen url: URL) -> Bool {
         return GIDSignIn.sharedInstance().handle(url)
     }
     
     //追加
-  func application(_ application : UIApplication,open url: URL, sourceApplication: String?, annotation: Any)->Bool{
-      return ApplicationDelegate.shared.application(application, open: url, sourceApplication: sourceApplication, annotation: annotation)
-  }
+    func application(_ application : UIApplication,open url: URL, sourceApplication: String?, annotation: Any)->Bool{
+        return ApplicationDelegate.shared.application(application, open: url, sourceApplication: sourceApplication, annotation: annotation)
+    }
     //追加
-  func applicationDidBecomeActive(_ application: UIApplication) {
-      AppEvents.activateApp()
-  }
-
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        AppEvents.activateApp()
+    }
+    
     // MARK: UISceneSession Lifecycle
-
+    
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-
+    
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
@@ -59,7 +59,7 @@ extension AppDelegate {
         //インスタンス化
         let notificationCenter = UNUserNotificationCenter.current()
         //delegateを自身に設定。（プッシュ通知のみ必要らしい？）
-//        notificationCenter.delegate = self
+        //        notificationCenter.delegate = self
         //許可を求める。
         notificationCenter.requestAuthorization(options: [.alert, .sound]) { granted, error in
             if let error = error {
@@ -82,7 +82,7 @@ extension AppDelegate {
 
 //Mark:UserNotificationDelegate
 extension AppDelegate: UNUserNotificationCenterDelegate {
-   
+    
     //Mark:ForeGround
     func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
         completionHandler([.alert, .badge, .sound])
