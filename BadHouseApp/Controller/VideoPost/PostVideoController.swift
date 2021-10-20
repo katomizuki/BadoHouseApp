53cf4bdc0264cde326cca74ec2e8901fd18d41c1
Katopan <76249494+katomizuki@users.noreply.github.com>
2021-10-20 13:45:50 +0900

    update videoPost

--- BadHouseApp/Controller/VideoPost/PostVideoController.swift
+++ BadHouseApp/Controller/VideoPost/PostVideoController.swift
@@ -5,32 +5,31 @@ import FirebaseStorage
 import Firebase
 
 class PostVideoController: UIViewController {
-    
-    //Mark:Properties
-    private let logoImage:UIImageView = {
+    // Mark Properties
+    private let logoImage: UIImageView = {
         let iv = UIImageView()
         iv.image = UIImage(named: Constants.ImageName.logoImage)
         return iv
     }()
-    private lazy var singleButton:UIButton = {
+    private lazy var singleButton: UIButton = {
         let button = RegisterButton(text: Badominton.single.rawValue)
         button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
         button.addTarget(self, action: #selector(handle), for: UIControl.Event.touchUpInside)
         return button
     }()
-    private lazy var doubleButton:UIButton = {
+    private lazy var doubleButton: UIButton = {
         let button = RegisterButton(text: Badominton.double.rawValue)
         button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
         button.addTarget(self, action: #selector(handle), for: UIControl.Event.touchUpInside)
         return button
     }()
-    private lazy var mixButton:UIButton = {
+    private lazy var mixButton: UIButton = {
         let button = RegisterButton(text: Badominton.mix.rawValue)
         button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
         button.addTarget(self, action: #selector(handle), for: UIControl.Event.touchUpInside)
         return button
     }()
-    private let label:UILabel = {
+    private let label: UILabel = {
         let label = ProfileLabel(title: "今日の1ラリーを投稿してみよう", num: 20)
         label.textColor = Constants.AppColor.OriginalBlue
         return label
@@ -39,24 +38,34 @@ class PostVideoController: UIViewController {
     let fileOutput = AVCaptureMovieFileOutput()
     var recordButton: UIButton!
     var isRecording = false
-    //Mark lifeCycle
+    // Mark lifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
         setupLayout()
     }
-    //Mark setupMethod
+    // Mark setupMethod
     private func setupLayout() {
-        let stackView = UIStackView(arrangedSubviews: [label,singleButton,doubleButton,mixButton])
+        let stackView = UIStackView(arrangedSubviews: [label, singleButton, doubleButton, mixButton])
         stackView.axis = .vertical
         stackView.spacing = 20
         stackView.distribution = .fillEqually
         view.addSubview(stackView)
         view.addSubview(logoImage)
-        stackView.anchor(top:logoImage.bottomAnchor,left:view.leftAnchor,right: view.rightAnchor,paddingTop: 30,paddingRight:30, paddingLeft: 30,height:300)
-        logoImage.anchor(top:view.safeAreaLayoutGuide.topAnchor,paddingTop: 70,centerX: view.centerXAnchor,width: 100,height: 100)
+        stackView.anchor(top: logoImage.bottomAnchor,
+                         left: view.leftAnchor,
+                         right: view.rightAnchor,
+                         paddingTop: 30,
+                         paddingRight: 30,
+                         paddingLeft: 30,
+                         height: 300)
+        logoImage.anchor(top: view.safeAreaLayoutGuide.topAnchor,
+                         paddingTop: 70,
+                         centerX: view.centerXAnchor,
+                         width: 100,
+                         height: 100)
     }
-    //Mark selector
-    @objc private func handle(sender:UIButton) {
+    // Mark selector
+    @objc private func handle(sender: UIButton) {
 #if targetEnvironment(simulator)
         // do nothing
 #else
--- BadHouseApp/Controller/VideoPost/VideoCameraController.swift
+++ BadHouseApp/Controller/VideoPost/VideoCameraController.swift
@@ -5,23 +5,23 @@ import FirebaseStorage
 import Firebase
 
 class VideoCameraController: UIViewController {
-    //Mark properties
+    // Mark properties
     var keyWord = String()
     let captureSession = AVCaptureSession()
     let fileOutput = AVCaptureMovieFileOutput()
     var recordButton: UIButton!
     var isRecording = false
-    //Mark lifecycle
+    // Mark lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpCamera()
     }
-    //Mark setupMethod
+    // Mark setupMethod
     private func setUpCamera() {
         // デバイスの初期化
         let videoDevice: AVCaptureDevice? = AVCaptureDevice.default(for: AVMediaType.video)
         let audioDevice: AVCaptureDevice? = AVCaptureDevice.default(for: AVMediaType.audio)
-        //ビデオの画質
+        // ビデオの画質
         captureSession.sessionPreset = AVCaptureSession.Preset.medium
         // ビデオのインプット設定
         let videoInput: AVCaptureDeviceInput = try! AVCaptureDeviceInput(device: videoDevice!)
@@ -32,27 +32,26 @@ class VideoCameraController: UIViewController {
         captureSession.addOutput(fileOutput)
         captureSession.startRunning()
         // ビデオ表示
-        let videoLayer : AVCaptureVideoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        let videoLayer: AVCaptureVideoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
         videoLayer.frame = self.view.bounds
         videoLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
         self.view.layer.addSublayer(videoLayer)
         fileOutput.maxRecordedDuration = CMTimeMake(value: 60, timescale: 2)
         let connection = fileOutput.connection(with: .video)
         if fileOutput.availableVideoCodecTypes.contains(.hevc) {
-            fileOutput.setOutputSettings([AVVideoCodecKey:AVVideoCodecType.hevc], for: connection!)
+            fileOutput.setOutputSettings([AVVideoCodecKey: AVVideoCodecType.hevc], for: connection!)
         }
         // 録画ボタン
         self.recordButton = UIButton(frame: CGRect(x: 0, y: 0, width: 80, height: 80))
         self.recordButton.backgroundColor = .white
         self.recordButton.layer.masksToBounds = true
         self.recordButton.layer.cornerRadius = 80 / 2
-        self.recordButton.layer.position = CGPoint(x: self.view.bounds.width / 2, y:self.view.bounds.height - 120)
+        self.recordButton.layer.position = CGPoint(x: self.view.bounds.width / 2, y: self.view.bounds.height - 120)
         self.recordButton.addTarget(self, action: #selector(self.tappedRecordButton(sender:)), for: .touchUpInside)
         self.view.addSubview(recordButton)
     }
-    
-    //Mark selector
-    @objc func tappedRecordButton(sender: UIButton) {
+// Mark selector
+    @objc private func tappedRecordButton(sender: UIButton) {
         if isRecording {
             // 録画終了
             fileOutput.stopRecording()
@@ -63,20 +62,19 @@ class VideoCameraController: UIViewController {
             // 録画開始
             let path = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
             let documentDirectory = path[0] as String
-            let filePath:String = "\(documentDirectory)/.mp4"
-            let fileURL:NSURL = NSURL(fileURLWithPath: filePath)
-            fileOutput.startRecording(to: fileURL as URL, recordingDelegate: self)
+            let filePath: String = "\(documentDirectory)/.mp4"
+            let fileURL: NSURL = NSURL(fileURLWithPath: filePath)
+            let url = fileURL as URL
+            fileOutput.startRecording(to: url, recordingDelegate: self)
             self.isRecording = true
             sender.backgroundColor = Constants.AppColor.OriginalBlue
         }
     }
 }
-//Mark AVfileoutputDelegate
+// Mark AVfileoutputDelegate
 extension VideoCameraController: AVCaptureFileOutputRecordingDelegate {
-    
     func fileOutput(_ output: AVCaptureFileOutput, didFinishRecordingTo outputFileURL: URL, from connections: [AVCaptureConnection], error: Error?) {
-        StorageService.sendVideoData(videoUrl: outputFileURL, senderId: AuthService.getUserId(), keyWord:self.keyWord)
+        StorageService.sendVideoData(videoUrl: outputFileURL, senderId: AuthService.getUserId(), keyWord: self.keyWord)
         LocalNotificationManager.setNotification(3, of: .seconds, repeats: false, title: "プレ-を投稿しました", body: "")
     }
 }
-
