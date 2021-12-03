1361a5a92be2b83624c32cf66daa0824a108bbfd
Katopan <76249494+katomizuki@users.noreply.github.com>
2021-12-03 23:19:14 +0900

    update UI

--- BadHouseApp/Controller/Event/MakeEventController.swift
+++ BadHouseApp/Controller/Event/MakeEventController.swift
@@ -1,11 +1,13 @@
 import UIKit
 import RxSwift
 import RxCocoa
+import RxGesture
 import Firebase
 
 final class MakeEventController: UIViewController {
     // MARK: - Properties
     @IBOutlet private weak var titleTextField: UITextField!
+    @IBOutlet private weak var nextButton: UIButton!
     @IBOutlet private weak var circleSegment: UISegmentedControl! {
         didSet {
             circleSegment.addTarget(self, action: #selector(segmentTap(sender:)), for: UIControl.Event.valueChanged)
@@ -23,17 +25,11 @@ final class MakeEventController: UIViewController {
     // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupDelegate()
         setupBinding()
-        setupOwnTeamData()
     }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         self.setupNavAccessory()
-    }
-    // MARK: - SetupMethod
-    private func setupDelegate() {
-        titleTextField.delegate = self
     }
 
     private func setupBinding() {
@@ -46,35 +42,29 @@ final class MakeEventController: UIViewController {
                 let title = text ?? ""
                 self.eventTitle = title
                 self.eventBinding.titleTextInput.onNext(title)
-            }
-            .disposed(by: disposeBag)
+            }.disposed(by: disposeBag)
+        
+        nextButton.rx.tap.asDriver().drive(onNext: { [weak self] _ in
+            let storyboard = UIStoryboard(name: "MakeEvent", bundle: nil)
+            guard let controller = storyboard.instantiateViewController(withIdentifier: "MakeEventSecondController") as? MakeEventSecondController else { return }
+            self?.navigationController?.pushViewController(controller, animated: true)
+        }).disposed(by: disposeBag)
+        
+        noImageView.rx.tapGesture()
+            .when(.recognized)
+            .subscribe { [weak self]_ in
+                let pickerView = UIImagePickerController()
+                pickerView.delegate = self
+                self?.present(pickerView, animated: true, completion: nil)
+            }.disposed(by: disposeBag)
 
-    }
-    private func setupOwnTeamData() {
-        let uid = AuthService.getUserId()
-        UserService.getOwnTeam(uid: uid) { [weak self] teamIds in
-            guard let self = self else { return }
-            self.fetchData.fetchMyTeamData(idArray: teamIds)
-        }
     }
     // MARK: - SelectorMethod
     @objc private func segmentTap(sender: UISegmentedControl) {
         let index = sender.selectedSegmentIndex
         kindCircle = BadmintonCircle(rawValue: index)?.name
     }
-    @objc private func handleTap() {
-        titleTextField.resignFirstResponder()
-        notTitleLabel.isHidden = titleTextField.text?.isEmpty == true ? false : true
-    }
-    // MARK: IBAction
-    @IBAction private func plusImage(_ sender: Any) {
-        print(#function)
-        let pickerView = UIImagePickerController()
-        pickerView.delegate = self
-        self.present(pickerView, animated: true, completion: nil)
-    }
 }
-
 // MARK: - UInavigationDelegate
 extension MakeEventController: UINavigationControllerDelegate, UIImagePickerControllerDelegate {
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
@@ -85,9 +75,3 @@ extension MakeEventController: UINavigationControllerDelegate, UIImagePickerCont
     }
 }
 
-// MARK: - textFieldDelegate
-extension MakeEventController: UITextFieldDelegate {
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        textField.resignFirstResponder()
-    }
-}
---
+++ BadHouseApp/Controller/Event/MakeEventSecondController.swift
@@ -0,0 +1,29 @@
+//
+//  MakeEventSecondController.swift
+//  BadHouseApp
+//
+//  Created by ミズキ on 2021/12/03.
+//
+
+import UIKit
+
+class MakeEventSecondController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
