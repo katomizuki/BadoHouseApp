4da4a9d74ece81caf800c362647ad77c35ac5ec3
Katopan <76249494+katomizuki@users.noreply.github.com>
2022-02-24 07:56:31 +0900

    viewModelのリファクタリング

--- BadHouseApp/ViewModel/ScheduleViewModel.swift
+++ BadHouseApp/ViewModel/ScheduleViewModel.swift
@@ -4,11 +4,13 @@ import RxSwift
 protocol ScheduleViewModelInputs {
     func willAppear()
     func deleteSchdule(_ index: Int)
+    var errorInput: AnyObserver<Bool> { get }
+    var reloadInput: AnyObserver<Void> { get }
 }
 
 protocol ScheduleViewModelOutputs {
-    var isError: PublishSubject<Bool> { get }
-    var reload: PublishSubject<Void> { get }
+    var isError: Observable<Bool> { get }
+    var reload: Observable<Void> { get }
     var practiceList: BehaviorRelay<[Practice]> { get }
 }
 
@@ -17,15 +19,14 @@ protocol ScheduleViewModelType {
     var outputs: ScheduleViewModelOutputs { get }
 }
 
-final class ScheduleViewModel: ScheduleViewModelType, ScheduleViewModelInputs, ScheduleViewModelOutputs {
+final class ScheduleViewModel: ScheduleViewModelType {
   
     var inputs: ScheduleViewModelInputs { return self }
     var outputs: ScheduleViewModelOutputs { return self }
     
     private let userAPI: UserRepositry
     var practiceAPI: PracticeRepositry
-    var isError = PublishSubject<Bool>()
-    var reload = PublishSubject<Void>()
+    
     var user: User
     var practiceList = BehaviorRelay<[Practice]>(value: [])
     private let disposeBag = DisposeBag()
@@ -41,12 +42,34 @@ final class ScheduleViewModel: ScheduleViewModelType, ScheduleViewModelInputs, S
     func willAppear() {
         userAPI.getMyJoinPractice(user: user).subscribe {[weak self] practices in
             self?.practiceList.accept(practices)
-            self?.reload.onNext(())
+            self?.reloadInput.onNext(())
         } onFailure: { [weak self] _ in
-            self?.isError.onNext(true)
+            self?.errorInput.onNext(true)
         }.disposed(by: disposeBag)
     }
     func deleteSchdule(_ row: Int) {
         //  ここで削除する
+    }
+}
+
+extension ScheduleViewModel: ScheduleViewModelInputs {
+
+    var errorInput: AnyObserver<Bool> {
+        errorStream.asObserver()
+    }
+
+    var reloadInput: AnyObserver<Void> {
+        reloadStream.asObserver()
+    }
+}
+
+extension ScheduleViewModel: ScheduleViewModelOutputs {
+
+    var isError: Observable<Bool> {
+        errorStream.asObservable()
+    }
+
+    var reload: Observable<Void> {
+        reloadStream.asObservable()
     }
 }
